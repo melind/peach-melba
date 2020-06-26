81d828b0fabf0206bbd23458c8ff9f33a0a659bd
Mélinda M <melinda93@hotmail.fr>
2020-06-27 01:51:40 +0200

    test de design

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,13 +3,26 @@ import './index.css';
 
 import logo1 from '../../icones/logo1.svg';
 import logo2 from '../../icones/logo2.svg';
-
+import fond from '../../icones/fond.svg';
+/* <div id="fond">  </div>
+  <p id="p">P</p><p id="e">e</p><p id="c">c</p><p id="h">h</p><p id="ee">e</p><p id="point">.</p>
+                 <p id="m">m</p><p id="eee">e</p><p id="l">l</p><p id="b">b</p><p id="a">a</p><p id="()">()</p> */
 const Home = () => {
 
     return (
         <div className="home">
-         <div className="title">Peche.melba() </div>
-         <div> <img src={logo1} width="220"/> </div>
+       
+         <div className="title">
+             <h1 className="t">
+                <p id="peche">Peche.</p><p id="melba">melba</p>
+               
+             </h1></div>
+
+         <div className="logo">
+         <div className="logo1"> <img src={logo1} width="220"/> </div>
+         <div className="logo2"> <img src={logo2} width="220"/> </div>
+         </div>
+
         </div>
     )
 }
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,13 +1,108 @@
-import React from 'react';
+/* global THREE */ 
+import * as THREE from 'three';
+import React, { useEffect } from 'react';
 
 import './index.css'
 
+
+//main();
+
+
+
 const NotFound = () => {
+
+    function main() {
+
+  const canvas = document.querySelector('#peach');
+  const renderer = new THREE.WebGLRenderer({canvas});
+
+  const fov = 75;
+  const aspect = 2;  // valeur par défaut du canevas
+  const near = 0.1;
+  const far = 100;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 30;
+  const scene = new THREE.Scene();
+    scene.background = new THREE.Color('#5b0025');
+
+  {
+  const color = 0xf79203;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+  }
+    
+    const radius = 17;
+    const widthSegments = 80;
+    const heightSegments = 100;
+    
+    const geometry= new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments);
+
+    
+     const material = new THREE.MeshStandardMaterial({color: 0xdf4f15, roughness: 0.6, metalness: 0.1});
+
+     const peach = new THREE.Mesh(geometry, material);
+    
+
+    scene.add(peach);
+    renderer.render(scene, camera);
+  /*   
+  //const material = new THREE.MeshBasicMaterial({color: 0x44aa88});//without light
+  const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // cyan
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  renderer.render(scene, camera);*/
+
+
+
+    function resizeRendererToDisplaySize(renderer) {
+      const canvas = renderer.domElement;
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      const needResize = canvas.width !== width || canvas.height !== height;
+      if (needResize) {
+        renderer.setSize(width, height, false);
+      }
+      return needResize;
+    }
+  function render(time) {
+    time *= 0.001;  // convert time to seconds
+      if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      const pixelRatio = window.devicePixelRatio;
+      const width  = canvas.clientWidth  * pixelRatio | 0;
+      const height = canvas.clientHeight * pixelRatio | 0;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+  }
+
+    /*cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * .1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });*/
+   
+       peach.rotation.x = time;
+      peach.rotation.y = time;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+
+}
+    useEffect(() => {
+    main();
+    }, []);
 
     return (
         <div className="ntf"> 
         <h1>   "404" </h1>
-         
+       
+         <canvas id="peach"> </canvas>
         </div>
     )
 }
